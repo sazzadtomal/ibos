dafb78592ad43b6f1434487f259a4d4403840e93
sazzadtomal <sazzadtomalfordesktop1660@gmail.com>
2024-09-06 16:31:34 +0600

    bug fix in footerlist

--- src/components/FooterList.jsx
+++ src/components/FooterList.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const FooterList = ({header,items}) => {
   return (
     <div className="text-[1.8rem]">
-                <h3 className="text-white font-semibold ">{header}</h3>
+                <h3 className="text-white font-semibold mb-4">{header}</h3>
                 <ul className="text-[#81859F]">
                   {items.map((item)=><li key={item}>{item}</li>)}
                 </ul>
